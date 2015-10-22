a8cef36951009976fbb43f16bbfa8c0471e736df
santoshm <santosh.ece06@gmail.com>
2015-10-22 14:37:10 +0530

    COMMIT_1

---
+++ app/src/main/java/com/affiuxs/util/MAIN.java
@@ -0,0 +1,14 @@
+package com.affiuxs.util;
+
+/**
+ * Created by santoshm on 22/10/15.
+ */
+public class MAIN {
+    public static void main(String args[]) {
+        String str = "HELLO";
+        System.out.println("HELLO ANDORID WORLD!!");
+
+
+
+    }
+}
